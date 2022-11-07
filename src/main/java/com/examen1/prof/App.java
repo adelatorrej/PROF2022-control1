0cf7cf1aedbc13b1bc92c96f640fbcc80ef392c8
Alberto <a.delatorrej@alumnos.upm.es>
2022-11-07 18:18:55 +0100

    Upload project

---
+++ src/main/java/com/examen1/prof/App.java
@@ -0,0 +1,13 @@
+package com.examen1.prof;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
