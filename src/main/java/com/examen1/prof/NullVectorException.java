022e47ae1b41a2269324b88e735b6733b235bec6
Alberto <a.delatorrej@alumnos.upm.es>
2022-11-07 18:56:12 +0100

    First changes

---
+++ src/main/java/com/examen1/prof/NullVectorException.java
@@ -0,0 +1,6 @@
+package com.examen1.prof;
+
+@SuppressWarnings("serial")
+public class NullVectorException extends Exception{
+
+}
--- src/test/java/com/examen1/prof/AppTest.java
+++ src/test/java/com/examen1/prof/AppTest.java
@@ -1,38 +1,52 @@
 package com.examen1.prof;
 
+/*
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+*/
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.util.Vector;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+
+import org.junit.jupiter.api.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest //extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	@BeforeAll
+	static public void beforeAll() {
+		System.out.println("Executed before any test has been run");
+	}
+	
+	@AfterAll
+	static public void afterAll() {
+		System.out.println("Executed after all tests have been run");
+	}
+		
+	@Test
+	public void is_null_vector() throws NullVectorException{
+		//Vector<Asignatura> vectorAsignaturas= new Vector<Asignatura>();
+		Matricula m = new Matricula(null);
+		assertEquals(0,m.vectorAsignaturas.size());
+	}
+	
+	/*
+	@Test
+	public void myTestMethod(){
+		  Matricula m = mock(Matricula.class);
+		  when(m.getImporte()).thenReturn(<value>);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		  // THIS SHOULD BE TRUE
+		  assertEquals(<value>, c.<method>());
+	}
+	*/
+    
 }
